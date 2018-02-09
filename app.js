1b50bd6f89c9921ce846e9dffd3e981dc834d562
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-09 19:05:40 +0530

    caching balances for ost, ost prime, bt, airdroopped

--- app.js
+++ app.js
@@ -129,6 +129,6 @@ app.use(function (err, req, res, next) {
   return responseHelper.error('500', 'Something went wrong').renderResponse(res, 500);
 });
 
-app.listen(3000, function(){console.log('Example app listening on port 3000!')});
+app.listen(4001, function(){console.log('Example app listening on port 4001!')});
 
 //module.exports = app;
