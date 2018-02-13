ffa6c32e7817827b0e49a5551433e93d2e7e5a83
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-02-13 18:23:21 +0530

    fix

--- app/models/client.js
+++ app/models/client.js
@@ -5,14 +5,12 @@ var rootPrefix = '../..'
   , QueryDBKlass = require(rootPrefix + '/app/models/queryDb')
   ;
 
-<<<<<<< HEAD:app/models/client.js
 /*
  * Table configuration methods
  */
-const dbName = "company_client_"+coreConstants.ENVIRONMENT
-=======
+
 const dbName = "company_client_economy_"+coreConstants.SUB_ENVIRONMENT+"_"+coreConstants.ENVIRONMENT
->>>>>>> Code review changes:app/models/client_token.js
+
   , QueryDB = new QueryDBKlass(dbName)
   , tableName = 'clients'
   ;
