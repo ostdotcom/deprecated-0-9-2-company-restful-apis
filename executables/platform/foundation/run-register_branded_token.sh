7a49aef8285eb2542131766a432c32354cfa4779
aman_barbaria <aman@pepo.com>
2018-02-28 17:26:30 +0530

    script to run for company and foundation

---
+++ executables/platform/foundation/run-register_branded_token.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+node ./node_modules/@openstfoundation/openst-platform/executables/inter_comm/register_branded_token.js >> ./log/platform-executables-register_branded_token.log
---
+++ executables/platform/foundation/run-stake_and_mint.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+node ./node_modules/@openstfoundation/openst-platform/executables/inter_comm/stake_and_mint.js >> ./log/platform-executables-stake_and_mint.log
