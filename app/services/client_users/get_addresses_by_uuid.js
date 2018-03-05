182108b96b7b3fe3c3a6ce26e25f1912ea6d2c25
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-05 20:12:23 +0530

    copy related changes

---
+++ app/services/client_users/get_addresses_by_uuid.js
@@ -0,0 +1,61 @@
+"use strict";
+
+const rootPrefix = '../../..'
+    , responseHelper = require(rootPrefix + '/lib/formatter/response')
+    , logger = require(rootPrefix + '/lib/logger/custom_console_logger')
+    , ManagedAddressKlass = require(rootPrefix + '/app/models/managed_address')
+    , managedAddress = new ManagedAddressKlass()
+;
+
+/**
+ * constructor
+ *
+ * @constructor
+ */
+const GetAddressesByUuidKlass = function (params) {
+
+  const oThis = this;
+
+  oThis.uuids = params.uuids;
+  oThis.clientId = params.client_id;
+  
+};
+
+GetAddressesByUuidKlass.prototype = {
+
+  /**
+   * fetch data
+   *
+   * @return {Promise<result>}
+   */
+  perform: async function () {
+    
+    const oThis = this;
+    
+    var usersData = await managedAddress.getByUuids(oThis.uuids);
+
+    if (usersData.length <= 0) {
+      return Promise.resolve(responseHelper.error("s_cu_gabu_1", "No Data found"));
+    }
+    
+    var response = {};
+
+    for (var i = 0; i < usersData.length; i++) {
+      
+      var user = usersData[i];
+
+      if (user['client_id'] != oThis.clientId) {
+        return Promise.resolve(responseHelper.error("s_cu_gabu_2", "Invalid client details."));
+      }
+
+      response[user['uuid']] = user['ethereum_address'];
+      
+    }
+
+    return Promise.resolve(responseHelper.successWithData({user_addresses: response}));
+
+  }
+
+};
+
+module.exports = GetAddressesByUuidKlass;
