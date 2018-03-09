f0ba7ae1bdbc52d3ddc51664e1ab55e88d6ce529
kedarchandrayan <kedar@pepo.com>
2018-03-09 13:23:30 +0530

    intercoms executables for stake / mint / register

---
+++ executables/inter_comm/register_branded_token.js
@@ -0,0 +1,35 @@
+"use strict";
+
+/**
+ * This executable / script is intermediate communicator between value chain and utility chain used for the registering branded token.
+ *
+ * <br>It listens to the ProposedBrandedToken event emitted by proposeBrandedToken method of openSTUtility contract.
+ * On getting this event, it calls registerBrandedToken method of utilityRegistrar contract followed
+ * by calling registerUtilityToken method of valueRegistrar contract.
+ *
+ * @module executables/inter_comm/register_branded_token
+ */
+
+
+const rootPrefix = '../..'
+;
+
+//Always Include Module overrides First
+require(rootPrefix + '/module_overrides/index');
+
+
+const openStPlatform = require('@openstfoundation/openst-platform')
+;
+
+const logger = require(rootPrefix + '/helpers/custom_console_logger')
+  , RegisterBrandedTokenInterComm = openStPlatform.services.interComm.registerBrandedToken
+;
+
+const args = process.argv
+  , filePath = args[2]
+;
+
+const registerBrandedTokenInterCommObj = new RegisterBrandedTokenInterComm({file_path: filePath});
+registerBrandedTokenInterCommObj.registerInterruptSignalHandlers();
+registerBrandedTokenInterCommObj.init();
+logger.win("InterComm Script for Register Branded Token initiated.");
---
+++ executables/inter_comm/stake_and_mint_processor.js
@@ -0,0 +1,36 @@
+"use strict";
+
+/**
+ * This service is intermediate communicator between value chain and utility chain used for process staking and process minting.
+ *
+ * <br>It listens to the StakingIntentConfirmed event emitted by confirmStakingIntent method of openSTUtility contract.
+ * On getting this event, it calls processStaking method of openStValue contract
+ * followed by calling processMinting method of openStUtility contract
+ * followed by calling claim of branded token contract / simple token prime contract.
+ *
+ * @module executables/inter_comm/stake_and_mint_processor
+ */
+
+const rootPrefix = '../..'
+;
+
+//Always Include Module overrides First
+require(rootPrefix + '/module_overrides/index');
+
+const openStPlatform = require('@openstfoundation/openst-platform')
+;
+
+const rootPrefix = '../..'
+  , logger = require(rootPrefix + '/helpers/custom_console_logger')
+  , StakeAndMintProcessorInterCommKlass = openStPlatform.services.interComm.stakeAndMintProcessor
+;
+
+const args = process.argv
+  , filePath = args[2]
+;
+
+const stakeAndMintProcessorInterCommObj = new StakeAndMintProcessorInterCommKlass({file_path: filePath});
+stakeAndMintProcessorInterCommObj.registerInterruptSignalHandlers();
+stakeAndMintProcessorInterCommObj.init();
+
+logger.win("InterComm Script for Stake and Mint Processor initiated.");
