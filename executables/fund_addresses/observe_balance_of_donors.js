ec2e88833263d886d03af236917a11cefe4f27bf
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-03-10 10:51:25 +0530

    added new cron to observer balances of critical addresses

---
+++ executables/fund_addresses/observe_balance_of_donors.js
@@ -0,0 +1,115 @@
+"use strict";
+
+/**
+ * Observe balances of Donors
+ *
+ * @module executables/fund_addresses/observe_balance_of_donors
+ */
+
+const rootPrefix = "../.."
+    , logger = require(rootPrefix + "/lib/logger/custom_console_logger")
+    , chainInteractionConstants = require(rootPrefix + '/config/chain_interaction_constants')
+;
+
+/**
+ *
+ * @constructor
+ */
+const balanceObserververKlass = function () {
+
+  const oThis = this;
+
+  oThis.notifyLogs = {};
+
+};
+
+balanceObserververKlass.prototype = {
+
+  perform: async function() {
+
+    const oThis = this;
+
+    await _observeFoundationEthBalance();
+
+    await _observeUtilityChainOwnerEthBalance();
+
+    if (Object.keys(oThis.notifyLogs) > 0) {
+
+      logger.notify('e_fa_1', 'Critical: Balances Too Low For Critical Addresses', oThis.notifyLogs);
+
+    }
+
+  },
+
+  /**
+   * fetch eth balance of foundation as it grants Eth to users after signup
+   *
+   * @return {Promise}
+   */
+  _observeFoundationEthBalance: async function() {
+
+    const oThis = this
+        , foundationAddr = chainInteractionConstants.FOUNDATION_ADDR
+        , ethBalanceRsp = await oThis._fetchEthBalance(foundationAddr);
+
+    if (ethBalanceRsp.isSuccess()) {
+
+      const ethBalance = ethBalanceRsp.data.balance;
+
+      if (ethBalance < _minFoundationEthBalance) {
+        oThis.notifyLogs['foundationEthBalance'] = 'FoundationEthBalance is less than : ' + _minFoundationEthBalance + ' ETH';
+      }
+
+    }
+
+  },
+
+  /**
+   * fetch eth balance of utility chain owner as it grants Eth to several other internal addresses
+   *
+   * @return {Promise}
+   */
+  _observeUtilityChainOwnerEthBalance: async function() {
+
+    const oThis = this
+        , utilityChainOwnerAddr = chainInteractionConstants.UTILITY_CHAIN_OWNER_ADDR
+        , ethBalanceRsp = await oThis._fetchEthBalance(utilityChainOwnerAddr);
+
+    if (ethBalanceRsp.isSuccess()) {
+
+      const ethBalance = ethBalanceRsp.data.balance;
+
+      if (ethBalance < _minUtilityChainOwnerEthBalance) {
+        oThis.notifyLogs['utilityChainOwnerEthBalance'] = 'UtilityChainOwnerEthBalance is less than : ' + _minUtilityChainOwnerEthBalance + ' ETH';
+      }
+
+    }
+
+  },
+
+  /**
+   * fetch eth balance
+   *
+   * @return {Promise}
+   */
+  _fetchEthBalance: function (address) {
+
+    const oThis = this;
+
+    const obj = new openStPlatform.services.balance.eth({'address': address});
+
+    return obj.perform();
+
+  },
+
+  // thresholds
+
+  _minFoundationEthBalance: 10,
+
+  _minUtilityChainOwnerEthBalance: 10
+
+}
+
+// perform action
+const balanceObserververKlassObj = new balanceObserververKlass();
+balanceObserververKlassObj.perform();
