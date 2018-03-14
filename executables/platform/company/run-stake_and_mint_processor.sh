0ed6be56abcd2e370f486f43f046a1b8f6efdcd9
kedarchandrayan <kedar@pepo.com>
2018-03-14 22:37:44 +0530

    minor.

--- executables/platform/company/run-stake_and_mint_processor.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-NODE_EXEC=$(which node);
-$NODE_EXEC ./node_modules/@openstfoundation/openst-platform/executables/inter_comm/stake_and_mint_processor.js $1;
--- executables/platform/foundation/run-register_branded_token.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-NODE_EXEC=$(which node);
-$NODE_EXEC ./node_modules/@openstfoundation/openst-platform/executables/inter_comm/register_branded_token.js $1;
