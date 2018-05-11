d975dc5685809d298fb736f6dd08866c444a97fb
kedarchandrayan <kedar@pepo.com>
2018-05-11 16:58:22 +0530

    transfers related changes in block scanner.

--- executables/block_scanner/execute_transaction.js
+++ executables/block_scanner/execute_transaction.js
@@ -101,7 +101,7 @@ BlockScannerBaseKlass.prototype = {
 
         await oThis.shortlistTransactions(currentBlock.transactions);
 
-        if(oThis.shortlistedTxObjs.length === 0) {
+        if (oThis.shortlistedTxObjs.length === 0) {
           oThis.updateScannerDataFile();
           return oThis.schedule();
         }
@@ -138,44 +138,56 @@ BlockScannerBaseKlass.prototype = {
     const oThis = this
       , batchSize = 100
       , waitingForMiningStatus = new TransactionLogModel().invertedStatuses[transactionLogConst.waitingForMiningStatus]
-      , tokenTransferTransactionType = new TransactionLogModel().invertedTransactionTypes[transactionLogConst.tokenTransferTransactionType]
-
+      , tokenTransferTransactionType = new TransactionLogModel().invertedTransactionTypes[
+      transactionLogConst.tokenTransferTransactionType]
+      , stpTransferTransactionType = new TransactionLogModel().invertedTransactionTypes[
+      transactionLogConst.stpTransferTransactionType]
     ;
 
-    var batchNo = 1;
+    var batchNo = 1
+      , totalBtTransfers = 0
+      , totalSTPTransfers = 0;
 
-    while(true) {
+    while (true) {
       const offset = (batchNo - 1) * batchSize
         , batchedTxHashes = allTxHashes.slice(offset, batchSize + offset)
       ;
 
       batchNo = batchNo + 1;
 
-      if(batchedTxHashes.length === 0) break;
+      if (batchedTxHashes.length === 0) break;
 
       const batchedTxLogRecords = await new TransactionLogModel()
         .getByTransactionHash(batchedTxHashes);
 
-      if(batchedTxLogRecords.length === 0) continue;
+      if (batchedTxLogRecords.length === 0) continue;
 
-      for(var i = 0; i < batchedTxLogRecords.length; i ++) {
+      for (var i = 0; i < batchedTxLogRecords.length; i++) {
 
         const currRecord = batchedTxLogRecords[i];
 
-        if(currRecord.status != waitingForMiningStatus) continue;
+        if (currRecord.status != waitingForMiningStatus) continue;
 
-        if(currRecord.transaction_type != tokenTransferTransactionType) continue;
+        if (currRecord.transaction_type != tokenTransferTransactionType) {
+          totalBtTransfers = totalBtTransfers + 1;
+        } else if (currRecord.transaction_type != stpTransferTransactionType) {
+          totalSTPTransfers = totalSTPTransfers + 1;
+        } else {
+          continue;
+        }
 
         oThis.shortlistedTxObjs.push(
           {
-            transaction_hash: currRecord.transaction_hash,
             id: currRecord.id,
-            input_params: currRecord.input_params
+            transaction_hash: currRecord.transaction_hash,
+            input_params: currRecord.input_params,
+            transaction_type: new TransactionLogModel().transactionTypes[currRecord.transaction_type]
           });
       }
     }
 
-    logger.log('Number of relevant transactions =', oThis.shortlistedTxObjs.length);
+    logger.log('Total BT Transfers:', totalBtTransfers);
+    logger.log('Total STP Transfers:', totalSTPTransfers);
 
     return Promise.resolve();
   },
@@ -187,7 +199,7 @@ BlockScannerBaseKlass.prototype = {
 
     var batchNo = 1;
 
-    while(true) {
+    while (true) {
       const offset = (batchNo - 1) * batchSize
         , batchedTxObjs = oThis.shortlistedTxObjs.slice(offset, batchSize + offset)
         , promiseArray = []
@@ -195,34 +207,37 @@ BlockScannerBaseKlass.prototype = {
 
       batchNo = batchNo + 1;
 
-      if(batchedTxObjs.length === 0) break;
+      if (batchedTxObjs.length === 0) break;
 
-      for(var i = 0; i < batchedTxObjs.length; i ++) {
+      for (var i = 0; i < batchedTxObjs.length; i++) {
         const currTxHash = batchedTxObjs[i].transaction_hash;
         promiseArray.push(oThis.web3Provider.eth.getTransactionReceipt(currTxHash))
       }
 
       const txReceiptResults = await Promise.all(promiseArray);
 
-      for(var i = 0; i < batchedTxObjs.length; i ++) {
+      for (var i = 0; i < batchedTxObjs.length; i++) {
         const txReceipt = txReceiptResults[i];
-        const decodedEvents = abiDecoder.decodeLogs(txReceipt.logs);
 
-        if (batchedTxObjs[i].input_params && batchedTxObjs[i].input_params.postReceiptProcessParams) {
-          const postAirdropParams = batchedTxObjs[i].input_params.postReceiptProcessParams;
-          const postAirdropPay = new PostAirdropPayKlass(postAirdropParams, decodedEvents,txReceipt.status);
-          const postAirdropPayResponse = await postAirdropPay.perform();
+        if (batchedTxObjs[i].transaction_type == transactionLogConst.tokenTransferTransactionType) {
+          const decodedEvents = abiDecoder.decodeLogs(txReceipt.logs);
 
-          if (postAirdropPayResponse.isSuccess()) {
-            delete batchedTxObjs[i].input_params.postReceiptProcessParams;
+          if (batchedTxObjs[i].input_params && batchedTxObjs[i].input_params.postReceiptProcessParams) {
+            const postAirdropParams = batchedTxObjs[i].input_params.postReceiptProcessParams;
+            const postAirdropPay = new PostAirdropPayKlass(postAirdropParams, decodedEvents, txReceipt.status);
+            const postAirdropPayResponse = await postAirdropPay.perform();
+
+            if (postAirdropPayResponse.isSuccess()) {
+              delete batchedTxObjs[i].input_params.postReceiptProcessParams;
+            }
           }
+
+          const eventData = oThis._getEventData(decodedEvents);
+
+          batchedTxObjs[i].commission_amount_in_wei = eventData._commissionTokenAmount;
+          batchedTxObjs[i].bt_transfer_in_wei = eventData._tokenAmount;
+          batchedTxObjs[i].gas_used = txReceipt.gasUsed;
         }
-
-        const eventData = oThis._getEventData(decodedEvents);
-
-        batchedTxObjs[i].commission_amount_in_wei = eventData._commissionTokenAmount;
-        batchedTxObjs[i].bt_transfer_in_wei = eventData._tokenAmount;
-        batchedTxObjs[i].gas_used = txReceipt.gasUsed;
 
         batchedTxObjs[i].status = parseInt(txReceipt.status, 16);
       }
@@ -243,7 +258,7 @@ BlockScannerBaseKlass.prototype = {
 
     var batchNo = 1;
 
-    while(true) {
+    while (true) {
 
       const offset = (batchNo - 1) * batchSize
         , batchedTxObjs = oThis.shortlistedTxObjs.slice(offset, batchSize + offset)
@@ -252,21 +267,27 @@ BlockScannerBaseKlass.prototype = {
 
       batchNo = batchNo + 1;
 
-      if(batchedTxObjs.length === 0) break;
+      if (batchedTxObjs.length === 0) break;
 
-      for(var i = 0; i < batchedTxObjs.length; i++) {
-
-        const formattedReceipt = {
-          commission_amount_in_wei: batchedTxObjs[i].commission_amount_in_wei,
-          bt_transfer_in_wei: batchedTxObjs[i].bt_transfer_in_wei,
-        };
+      for (var i = 0; i < batchedTxObjs.length; i++) {
+        let formattedReceipt = batchedTxObjs[i].transaction_type == transactionLogConst.tokenTransferTransactionType ?
+          {
+            commission_amount_in_wei: batchedTxObjs[i].commission_amount_in_wei,
+            bt_transfer_in_wei: batchedTxObjs[i].bt_transfer_in_wei,
+          } :
+          {};
 
         const status = (batchedTxObjs[i].status == 1) ? completeStatus : failedStatus;
 
         const promise = new TransactionLogModel().updateRecord(
           batchedTxObjs[i].id,
-          {status: status, formatted_receipt: formattedReceipt, input_params: batchedTxObjs[i].input_params,
-            block_number: oThis.currentBlock, gas_used: batchedTxObjs[i].gas_used}
+          {
+            status: status,
+            formatted_receipt: formattedReceipt,
+            input_params: batchedTxObjs[i].input_params,
+            block_number: oThis.currentBlock,
+            gas_used: batchedTxObjs[i].gas_used
+          }
         );
 
         promiseArray.push(promise);
@@ -310,12 +331,14 @@ BlockScannerBaseKlass.prototype = {
     // if the current block is far behind the highest block, schedule for 10 ms otherwise schedule for 2 s
     const waitInterval = (oThis.highestBlock - oThis.INTENTIONAL_BLOCK_DELAY <= oThis.scannerData.lastProcessedBlock) ? 2000 : 10;
 
-    logger.win('* Scheduled checkForNewBlocks after', waitInterval/1000.0, 'seconds.');
+    logger.win('* Scheduled checkForNewBlocks after', waitInterval / 1000.0, 'seconds.');
 
     logger.log('------------------------------------------------');
 
     setTimeout(
-      function () {oThis.checkForNewBlocks();},
+      function () {
+        oThis.checkForNewBlocks();
+      },
       waitInterval
     );
   },
@@ -328,7 +351,9 @@ BlockScannerBaseKlass.prototype = {
     ;
 
     setTimeout(
-      function () {oThis.init();},
+      function () {
+        oThis.init();
+      },
       1000
     );
   },
@@ -376,24 +401,24 @@ BlockScannerBaseKlass.prototype = {
   _getEventData: function (decodedEvents) {
     const eventData = {_tokenAmount: '0', _commissionTokenAmount: '0'};
 
-    if(!decodedEvents || decodedEvents.length === 0) {
+    if (!decodedEvents || decodedEvents.length === 0) {
       return eventData;
     }
 
     var airdropPaymentEventVars = null;
 
-    for(var i = 0; i < decodedEvents.length; i++) {
+    for (var i = 0; i < decodedEvents.length; i++) {
       if (decodedEvents[i].name == 'AirdropPayment') {
         airdropPaymentEventVars = decodedEvents[i].events;
         break;
       }
     }
 
-    if(!airdropPaymentEventVars || airdropPaymentEventVars.length === 0) {
+    if (!airdropPaymentEventVars || airdropPaymentEventVars.length === 0) {
       return eventData;
     }
 
-    for(var i = 0; i < airdropPaymentEventVars.length; i++) {
+    for (var i = 0; i < airdropPaymentEventVars.length; i++) {
       if (airdropPaymentEventVars[i].name == '_commissionTokenAmount') {
         eventData._commissionTokenAmount = airdropPaymentEventVars[i].value;
       }
@@ -407,7 +432,7 @@ BlockScannerBaseKlass.prototype = {
   }
 };
 
-const usageDemo = function() {
+const usageDemo = function () {
   logger.log('usage:', 'node ./executables/block_scanner/execute_transaction.js processLockId datafilePath');
   logger.log('* processLockId is used for ensuring that no other process with the same processLockId can run on a given machine.');
   logger.log('* datafilePath is the path to the file which is storing the last block scanned info.');
@@ -425,13 +450,13 @@ const ProcessLocker = new ProcessLockerKlass()
 ProcessLocker.canStartProcess({process_title: 'executables_block_scanner_execute_transaction' + processLockId});
 
 const validateAndSanitize = function () {
-  if(!processLockId) {
+  if (!processLockId) {
     logger.error('Process Lock id NOT passed in the arguments.');
     usageDemo();
     process.exit(1);
   }
 
-  if(!datafilePath) {
+  if (!datafilePath) {
     logger.error('Data file path is NOT passed in the arguments.');
     usageDemo();
     process.exit(1);
