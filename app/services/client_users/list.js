a7cae04c1d0408585d32e53ac7ea636764539d99
Puneet Khushwani <puneet.khushwani@pepo.com>
2018-05-16 12:06:40 +0530

    added validation od id passed while filtering user list

--- app/services/client_users/list.js
+++ app/services/client_users/list.js
@@ -238,6 +238,10 @@ listUserKlass.prototype = {
       oThis.uuidsForFiltering = basicHelper.commaSeperatedStrToArray(oThis.uuidsString);
       if (oThis.uuidsForFiltering.length > 100) {
         errors_object.push('invalid_id_user_list');
+      } else {
+        if (!basicHelper.isValidUuidArray(oThis.uuidsForFiltering)) {
+          errors_object.push('invalid_id_user_list');
+        }
       }
     }
 
