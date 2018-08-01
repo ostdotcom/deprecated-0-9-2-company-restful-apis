385e3985c990032bc54130f904a2f9ee6a0a4866
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-08-01 17:35:40 +0530

    Changes for incorporating instance composer in SaaS

--- routes/helper.js
+++ routes/helper.js
@@ -4,15 +4,17 @@ const rootPrefix = '..',
   responseHelper = require(rootPrefix + '/lib/formatter/response'),
   logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
   basicHelper = require(rootPrefix + '/helpers/basic'),
-  apiParamsValidator = require(rootPrefix + '/lib/validators/api_params');
+  apiParamsValidator = require(rootPrefix + '/lib/validators/api_params'),
+  ClientConfigStrategiesCache = require(rootPrefix + '/lib/cache_management/client_config_strategies'),
+  InstanceComposer = require(rootPrefix + '/instance_composer');
 
 const routeMethods = {
-  performer: async function(req, res, next, CallerKlass, errorCode, afterValidationFunc, dataFormatterFunc) {
+  performer: async function(req, res, next, CallerKlassGetter, errorCode, afterValidationFunc, dataFormatterFunc) {
     const oThis = this,
       errorConfig = basicHelper.fetchErrorConfig(req.decodedParams.apiVersion);
 
     return oThis
-      .asyncPerform(req, res, next, CallerKlass, afterValidationFunc, dataFormatterFunc)
+      .asyncPerform(req, res, next, CallerKlassGetter, afterValidationFunc, dataFormatterFunc)
       .catch(function(error) {
         if (responseHelper.isCustomResult(error)) {
           error.renderResponse(res, errorConfig);
@@ -30,7 +32,7 @@ const routeMethods = {
       });
   },
 
-  asyncPerform: async function(req, res, next, CallerKlass, afterValidationFunc, dataFormatterFunc) {
+  asyncPerform: async function(req, res, next, CallerKlassGetter, afterValidationFunc, dataFormatterFunc) {
     req.decodedParams = req.decodedParams || {};
 
     const oThis = this,
@@ -61,7 +63,13 @@ const routeMethods = {
       response.renderResponse(res, errorConfig);
     };
 
-    return new CallerKlass(req.serviceParams).perform().then(handleResponse);
+    let configStrategy = await oThis._fetchConfigStrategy(req.serviceParams['client_id']);
+
+    let instanceComposer = new InstanceComposer(configStrategy);
+
+    let Klass = CallerKlassGetter.apply(instanceComposer);
+
+    return new Klass(req.serviceParams).perform().then(handleResponse);
   },
 
   replaceKey: function(data, oldKey, newKey) {
@@ -74,6 +82,22 @@ const routeMethods = {
     data[newKey] = keyValue;
 
     return data;
+  },
+
+  _fetchConfigStrategy: async function(client_id) {
+    const oThis = this;
+
+    let configStrategy = {};
+
+    let clientConfigStrategiesCache = new ClientConfigStrategiesCache({ client_id: client_id });
+
+    let clientConfigStrategiesResponse = await clientConfigStrategiesCache.fetch();
+
+    let strategy_ids = clientConfigStrategiesResponse.data;
+
+    //TODO: Get strategy using ids from in-memory cache
+
+    return {};
   }
 };
 
