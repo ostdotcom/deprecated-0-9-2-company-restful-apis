1eb4d60f086983b7a2a8d228902e39303340a69c
ankitsingh <ankit@ost.com>
2018-08-01 15:41:42 +0530

    Added encryption and decryption calls.

--- app/models/config_strategy.js
+++ app/models/config_strategy.js
@@ -13,7 +13,8 @@ const rootPrefix = '../..',
   util = require(rootPrefix + '/lib/util'),
   localCipher = require(rootPrefix + '/lib/encryptors/local_cipher'),
   addressSaltCacheKlass = require(rootPrefix + '/lib/cache_management/managedAddressesSalt'),
-  inMemoryCacheInstance = require(rootPrefix + '/lib/cache_management/engine/in_memory');
+  inMemoryCacheInstance = require(rootPrefix + '/lib/cache_management/engine/in_memory'),
+  encryptorKlass = require(rootPrefix + '/lib/encryptors/addresses_encryptor');
 
 const dbName = 'saas_config_' + coreConstants.SUB_ENVIRONMENT + '_' + coreConstants.ENVIRONMENT;
 
@@ -47,19 +48,26 @@ const kinds = {
 const ConfigStrategyModelSpecificPrototype = {
   tableName: 'config_strategies',
 
+  /*
+  * inserts the config strategy params, kind, managed address salt and sha encryption of config strategy params.
+  *
+  * @return {Promise<integer>} - returns a Promise with integer of strategy id.
+  * returns: hash of all the parameters retrieved from database.
+  *
+  */
   insertStrategyKind: async function(params) {
     const oThis = this,
       configStrategyModelInstance = new ConfigStrategyModel(),
       kind = params.kind,
       configStrategyParams = params.config_strategy_params,
       managedAddressSaltId = params.managed_address_salt_id,
-      strategyKind = invertedKinds[kind];
+      strategyKind = invertedKinds[kind],
+      encryptorObj = new encryptorKlass({ managedAddressSaltId: managedAddressSaltId });
 
     let isParamPresent = [],
-      salt = oThis.getDecreptedSalt(managedAddressSaltId),
-      encryptedConfigStrategyParams = localCipher.encrypt(salt, configStrategyParams);
-
-    let hashedConfigStrategyParams = localCipher.getShaHashedText(JSON.stringify(configStrategyParams));
+      configStrategyParamsString = JSON.stringify(configStrategyParams),
+      encryptedConfigStrategyParams = await encryptorObj.encrypt(configStrategyParamsString),
+      hashedConfigStrategyParams = localCipher.getShaHashedText(configStrategyParamsString);
 
     await oThis
       .getStrategyIdByParams(configStrategyParams)
@@ -71,13 +79,14 @@ const ConfigStrategyModelSpecificPrototype = {
       });
 
     if (isParamPresent.length > 0) {
-      //If configStrategyParams is already present in database then id of that param is sent as the first and only element in the array.
+      //If configStrategyParams is already present in database then id of that param is sent
+      // as the first and only element in the array.
       return Promise.resolve(isParamPresent[0]);
     } else {
       const data = {
         kind: strategyKind,
         params: encryptedConfigStrategyParams,
-        managed_address_salts_id: 6006,
+        managed_address_salts_id: managedAddressSaltId,
         hashed_params: hashedConfigStrategyParams
       };
 
@@ -88,27 +97,27 @@ const ConfigStrategyModelSpecificPrototype = {
   },
 
   /*
-
+  * get complete ConfigStrategy hash by passing array of strategy ids.
   *
-  * strategyIds : array of all strategy ids whose parameters are required.
-  *
+  * @return {Promise<Hash>} - returns a Promise with an hash of config strategy.
   * returns: hash of all the parameters retrieved from database.
-  * */
-  getConfigByIds: async function(strategyIds) {
+  *
+  */
+  getConfigStrategyByIds: async function(strategyIds) {
     const oThis = this;
 
     if (strategyIds.length == 0) {
       return Promise.reject('strategy ID array is empty');
     } else {
       var queryResult = await oThis
-        .select('params')
+        .select(['params', 'managed_address_salts_id'])
         .where(['id IN (?)', strategyIds])
         .fire();
 
-      console.log('QueryResults:', queryResult);
       let finalResult = {};
       for (var i = 0; i < queryResult.length; i++) {
-        var localDecryptedParams = localCipher.decrypt(salt, queryResult[i].params);
+        const encryptorObj = new encryptorKlass({ managedAddressSaltId: queryResult[i].managed_address_salts_id });
+        var localDecryptedParams = await encryptorObj.decrypt(queryResult[i].params);
         var localJsonObj = JSON.parse(localDecryptedParams);
         Object.assign(finalResult, localJsonObj);
       }
@@ -119,16 +128,20 @@ const ConfigStrategyModelSpecificPrototype = {
     }
   },
 
+  /*
+  * Get strategy id by passing unencrypted params hash.<br><br>
+  *
+  * @return {Promise<array>} - returns a Promise with an array containing strategy id.
+  *
+  */
   getStrategyIdByParams: async function(params) {
     const oThis = this;
 
-    var returnValue = [];
+    var returnValue = [],
+      paramsHash = localCipher.getShaHashedText(JSON.stringify(params));
 
-    var paramsHash = localCipher.getShaHashedText(JSON.stringify(params));
-
-    let query = oThis.select('id').where({ hashed_params: paramsHash });
-
-    let queryResult = await query.fire();
+    let query = oThis.select('id').where({ hashed_params: paramsHash }),
+      queryResult = await query.fire();
 
     if (queryResult.length != 0) {
       returnValue.push(queryResult[0].id);
