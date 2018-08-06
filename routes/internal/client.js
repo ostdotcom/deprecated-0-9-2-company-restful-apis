54a707936d31895df18fb1e3a6fa77bc0ce38afc
Puneet Khushwani <puneet.khushwani@ost.com>
2018-08-06 16:59:34 +0530

    FetchClientStatsClass service router changes

--- routes/internal/client.js
+++ routes/internal/client.js
@@ -2,14 +2,15 @@ const express = require('express'),
   router = express.Router();
 
 const rootPrefix = '../..',
-  fetchStatsKlass = require(rootPrefix + '/app/services/client/fetch_stats'),
   routeHelper = require(rootPrefix + '/routes/helper');
+
+require(rootPrefix + '/app/services/client/fetch_stats');
 
 /* fetch stats for a client token */
 router.get('/fetch-stats', function(req, res, next) {
   req.decodedParams.apiName = 'fetch_client_stats';
 
-  routeHelper.performer(req, res, next, fetchStatsKlass, 'r_ct_1');
+  routeHelper.performer(req, res, next, 'getFetchClientStatsClass', 'r_ct_1');
 });
 
 module.exports = router;
