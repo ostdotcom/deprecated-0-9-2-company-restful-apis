1fb368c3676276deb5a7183c6e1a0b10f8309ea8
Puneet Khushwani <puneet.khushwani@ost.com>
2018-08-16 16:46:26 +0530

    ic changes in sync ddb balance with chain

--- executables/ddb_related_data_migrations/sync_ddb_balance_with_chain.js
+++ executables/ddb_related_data_migrations/sync_ddb_balance_with_chain.js
@@ -1,15 +1,16 @@
 'use strict';
 
-const openStPlatform = require('@openstfoundation/openst-platform'),
-  openStorage = require('@openstfoundation/openst-storage');
-
 const rootPrefix = '../..',
-  getBrandedTokenBalanceKlass = openStPlatform.services.balance.brandedTokenFromChain,
-  TokenBalanceModel = openStorage.TokenBalanceModel,
-  ddbServiceObj = require(rootPrefix + '/lib/dynamoDB_service'),
-  autoscalingServiceObj = require(rootPrefix + '/lib/auto_scaling_service'),
+  InstanceComposer = require(rootPrefix + '/instance_composer'),
   logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
   responseHelper = require(rootPrefix + '/lib/formatter/response');
+
+require(rootPrefix + '/lib/providers/platform');
+require(rootPrefix + '/lib/providers/storage');
+
+const args = process.argv,
+  config_file_path = args[2],
+  configStrategy = require(config_file_path);
 
 /**
  *
@@ -73,15 +74,20 @@ SyncDdbBalancesWithChain.prototype = {
    */
   _syncForClient: async function(erc20_address, userAddresses) {
     const oThis = this,
-      batchSize = 25;
+      batchSize = 25,
+      ic = new InstanceComposer(configStrategy),
+      platformProvider = ic.getPlatformProvider(),
+      openSTPlaform = platformProvider.getInstance(),
+      getBrandedTokenBalanceKlass = openSTPlaform.services.balance.brandedTokenFromChain,
+      storageProvider = ic.getStorageProvider(),
+      openSTStorage = storageProvider.getInstance(),
+      TokenBalanceModel = openSTStorage.model.TokenBalance;
 
     logger.info(`starting for erc20_address: ${erc20_address}`);
 
     let batchNo = 1,
       tokenBalanceObj = new TokenBalanceModel({
-        erc20_contract_address: erc20_address,
-        ddb_service: ddbServiceObj,
-        auto_scaling: autoscalingServiceObj
+        erc20_contract_address: erc20_address
       });
 
     while (true) {
@@ -106,7 +112,9 @@ SyncDdbBalancesWithChain.prototype = {
       for (let i = 0; i < batcheduserAddresses.length; i++) {
         let updateData = {
           ethereum_address: batcheduserAddresses[i],
-          settle_amount: promiseResponses[i].data.balance
+          settle_amount: promiseResponses[i].data.balance,
+          un_settled_debit_amount: '0',
+          pessimistic_settled_balance: promiseResponses[i].data.balance
         };
         let updateRsp = await tokenBalanceObj.set(updateData);
 
@@ -122,7 +130,7 @@ SyncDdbBalancesWithChain.prototype = {
 };
 
 // const jsonStr = '{"0x4F739d40dB7509Fac8789a9Ff7bd3f86132738E2":["0xdcb2ec51c8c4632ae0039befef861fd1de2cd616","0xb9f6d4c885e8653f66fbf4f1a87d2db0835c70e8","0x16b8e5c5cf4532969746d9c97c5c233b3647c105","0x06c18b69b13b38a2ed0d0c1146c2ed340ed4e7e5","0x8837741e2ede243927679e2b9c290243140dafc7","0x8187f120d3d2aa0fd32df04bc1cf8a148beadf1f","0x924efb75845b8a5b93e00f864391894360315bdb","0x79f720d773619edea6aa91ce9553983bf36ce87f","0xab3e3db1362b2b91663b22ff277f039eaf1b8cdb","0xfec3033dd1e9de348e13169ed7c53dd6e03b28a1","0xb5c0d3fc77ec5422c0a16cdf652c764d29df5961","0x53dd4f876e922877ba57921ee2f8200bc4ce9963","0x1cb5860593729a7ff4d0ca7d0bc101a4544b19a9"]}';
-const jsonStr = '{}';
+const jsonStr = '{"0x841f82a034d7a0453d915b11574b10afe0be35eb": ["0x069d4cd6bf876772ec474c0b2ef013bd93b0892f"]}';
 const object = new SyncDdbBalancesWithChain(JSON.parse(jsonStr));
 object
   .perform()
