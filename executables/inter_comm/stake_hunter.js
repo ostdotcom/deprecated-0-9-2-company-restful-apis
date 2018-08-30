2e1b800f2ab4e5a003c701be69e85e5aa5b27b4a
Dhananjay8 <dhananjay@ost.com>
2018-08-30 13:41:03 +0530

    added validations in hunter intercomm

--- executables/inter_comm/stake_hunter.js
+++ executables/inter_comm/stake_hunter.js
@@ -14,17 +14,49 @@ const rootPrefix = '../..';
 
 //Always Include Module overrides First
 require(rootPrefix + '/module_overrides/index');
+
 require(rootPrefix + '/lib/providers/platform');
 
-const logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
-  openStPlatform = oThis
-    .ic()
-    .getPlatformProvider()
-    .getInstance(),
-  StakeHunterInterCommKlass = openStPlatform.services.interComm.stakeHunter;
+const usageDemo = function() {
+  logger.log('usage:', 'node ./executables/inter_comm/stake_hunter.js blockNoFilePath configStrategyFilePath');
+};
+
+const InstanceComposer = require(rootPrefix + '/instance_composer'),
+  logger = require(rootPrefix + '/lib/logger/custom_console_logger');
 
 const args = process.argv,
-  filePath = args[2];
+  filePath = args[2],
+  configStrategyFilePath = args[3];
+
+let configStrategy = {};
+
+const validateAndSanitize = function() {
+  if (args.length < 4) {
+    logger.error('Invalid arguments !!!');
+    usageDemo();
+    process.exit(1);
+  }
+
+  if (!filePath) {
+    logger.error('filePath Not Found!!');
+    process.exit(1);
+  }
+
+  if (!configStrategyFilePath) {
+    logger.error('configStrategyFilePath Not Found!!');
+    process.exit(1);
+  }
+
+  configStrategy = require(configStrategyFilePath);
+};
+
+// validate and sanitize the input params
+validateAndSanitize();
+
+const ic = new InstanceComposer(configStrategy),
+  platformProvider = ic.getPlatformProvider(),
+  openStPlatform = platformProvider.getInstance(),
+  StakeHunterInterCommKlass = openStPlatform.services.interComm.stakeHunter;
 
 const stakeHunterInterCommObj = new StakeHunterInterCommKlass({ file_path: filePath });
 stakeHunterInterCommObj.registerInterruptSignalHandlers();
