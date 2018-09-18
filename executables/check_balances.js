637791ff59bbad04925e89780fdea660656141b2
Dhananjay8 <dhananjay@ost.com>
2018-09-18 15:10:45 +0530

    changed usage description in ddb related executables

--- executables/check_balances.js
+++ executables/check_balances.js
@@ -1,6 +1,17 @@
 'use strict';
 
-const rootPrefix = '../..',
+/**
+ * This script is used to check and compare DynamoDB balances with chain.
+ *
+ * Usage: node executables/check_balances.js clientIdsStr
+ *
+ * Command Line Parameters Description:
+ * clientIdsStr: It takes clientIds as input
+ *
+ * @module executables/ddb_related_data_migrations/check_balances
+ */
+
+const rootPrefix = '..',
   InstanceComposer = require(rootPrefix + '/instance_composer'),
   ConfigStrategyHelperKlass = require(rootPrefix + '/helpers/config_strategy'),
   configStrategyHelper = new ConfigStrategyHelperKlass(),
@@ -11,6 +22,28 @@ const rootPrefix = '../..',
 
 require(rootPrefix + '/lib/providers/platform');
 require(rootPrefix + '/lib/providers/storage');
+
+const usageDemo = function() {
+  logger.log('usage:', 'node ./executables/ddb_related_data_migrations/check_balances.js client_ids_str');
+};
+
+const args = process.argv,
+  clientIdsStr = args[2];
+
+let clientIdsArray = [];
+
+const validateAndSanitize = function() {
+  if (clientIdsStr) {
+    clientIdsArray = JSON.parse(clientIdsStr);
+  } else {
+    logger.error('clientIdsArray is NOT passed in the arguments.');
+    usageDemo();
+    process.exit(1);
+  }
+};
+
+// validate and sanitize the input params
+validateAndSanitize();
 
 /**
  *
@@ -195,24 +228,6 @@ CheckBalances.prototype = {
     return Promise.resolve(responseHelper.successWithData({}));
   }
 };
-
-const usageDemo = function() {
-  logger.log('usage:', 'node ./executables/ddb_related_data_migrations/check_balances.js client_ids_str');
-};
-
-const args = process.argv,
-  clientIdsStr = args[2];
-
-let clientIdsArray = [];
-
-const validateAndSanitize = function() {
-  if (clientIdsStr) {
-    clientIdsArray = JSON.parse(clientIdsStr);
-  }
-};
-
-// validate and sanitize the input params
-validateAndSanitize();
 
 const object = new CheckBalances({ client_ids: clientIdsArray });
 object
--- executables/sync_ddb_balance_with_chain.js
+++ executables/sync_ddb_balance_with_chain.js
@@ -1,20 +1,57 @@
 'use strict';
 
-const rootPrefix = '../..',
+/**
+ * This script is used to synchronize DynamoDB balances with Chain.
+ * In case DDB balances are messed up, we can sync these balances with actual chain balances.
+ *
+ * You can compare and find mismatched balances from this script: executables/check_balances.js
+ *
+ * Usage: node executables/sync_ddb_balance_with_chain.js configStrategyFilePath
+ *
+ * Command Line Parameters Description:
+ * configStrategyFilePath: path to the file which is storing the config strategy info.
+ *
+ * It takes erc20 address to ethereum adresses map as input params
+ *
+ * @module executables/sync_ddb_balance_with_chain
+ */
+
+const rootPrefix = '..',
   InstanceComposer = require(rootPrefix + '/instance_composer'),
   logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
-  responseHelper = require(rootPrefix + '/lib/formatter/response');
+  responseHelper = require(rootPrefix + '/lib/formatter/response'),
+  configStrategyHelper = require(rootPrefix + '/helpers/config_strategy');
 
 require(rootPrefix + '/lib/providers/platform');
 require(rootPrefix + '/lib/providers/storage');
+require(rootPrefix + '/lib/cache_multi_management/erc20_contract_address');
 
 const args = process.argv,
-  config_file_path = args[2],
-  configStrategy = require(config_file_path);
+  configStrategyFilePath = args[2];
+
+let configStrategy = {};
+
+// Usage demo.
+const usageDemo = function() {
+  logger.log('usage:', 'node ./executables/sync_ddb_balance_with_chain.js configStrategyFilePath');
+  logger.log('* configStrategyFilePath is the path to the file which is storing the config strategy info.');
+};
+
+// Validate and sanitize the command line arguments.
+const validateAndSanitize = function() {
+  if (!configStrategyFilePath) {
+    logger.error('Config strategy file path is NOT passed in the arguments.');
+    usageDemo();
+    process.exit(1);
+  }
+
+  configStrategy = require(configStrategyFilePath);
+};
+
+// Validate and sanitize the input params.
+validateAndSanitize();
 
 /**
- *
- * @param params
  *
  * @constructor
  *
@@ -72,7 +109,7 @@ SyncDdbBalancesWithChain.prototype = {
    *
    * @returns {promise}
    */
-  _syncForClient: async function(erc20_address, userAddresses) {
+  _syncForClient: async function(erc20Address, userAddresses) {
     const oThis = this,
       batchSize = 25,
       ic = new InstanceComposer(configStrategy),
@@ -81,28 +118,47 @@ SyncDdbBalancesWithChain.prototype = {
       getBrandedTokenBalanceKlass = openSTPlaform.services.balance.brandedTokenFromChain,
       storageProvider = ic.getStorageProvider(),
       openSTStorage = storageProvider.getInstance(),
-      TokenBalanceModel = openSTStorage.model.TokenBalance;
+      TokenBalanceModel = openSTStorage.model.TokenBalance,
+      Erc20ContractAddressCacheKlass = ic.getErc20ContractAddressCache();
 
-    logger.info(`starting for erc20_address: ${erc20_address}`);
+    let cacheObj = new Erc20ContractAddressCacheKlass({
+        addresses: [erc20Address],
+        chain_id: configStrategy.OST_UTILITY_CHAIN_ID
+      }),
+      cacheFetchRsp = await cacheObj.fetch();
+
+    if (cacheFetchRsp.isFailure()) {
+      return Promise.reject(cacheFetchRsp);
+    }
+
+    let erc20ContractAddressesData = cacheFetchRsp.data,
+      knownBTContractData = erc20ContractAddressesData[erc20Address],
+      clientId = knownBTContractData['client_id'];
+
+    let configStrategyHashRsp = await new configStrategyHelper({ clientId: clientId }).getConfigStrategy(clientId),
+      configStrategyHash = configStrategyHashRsp.data,
+      shardName = configStrategyHash.TOKEN_BALANCE_SHARD_NAME;
+
+    logger.info(`params: client_id: ${clientId}, shard_name: ${shardName}, erc20_address: ${erc20Address}`);
 
     let batchNo = 1,
       tokenBalanceObj = new TokenBalanceModel({
-        erc20_contract_address: erc20_address,
-        shard_name: configStrategy.TOKEN_BALANCE_SHARD_NAME
+        erc20_contract_address: erc20Address,
+        shard_name: shardName
       });
 
     while (true) {
       let offset = (batchNo - 1) * batchSize,
-        batcheduserAddresses = userAddresses.slice(offset, batchSize + offset);
+        batchedUserAddresses = userAddresses.slice(offset, batchSize + offset);
 
-      if (batcheduserAddresses.length === 0) break;
+      if (batchedUserAddresses.length === 0) break;
 
       let promises = [];
 
-      for (let i = 0; i < batcheduserAddresses.length; i++) {
+      for (let i = 0; i < batchedUserAddresses.length; i++) {
         let promise = new getBrandedTokenBalanceKlass({
-          address: batcheduserAddresses[i],
-          erc20_address: erc20_address,
+          address: batchedUserAddresses[i],
+          erc20_address: erc20Address,
           use_cache: 0
         }).perform();
         promises.push(promise);
@@ -110,9 +166,9 @@ SyncDdbBalancesWithChain.prototype = {
 
       let promiseResponses = await Promise.all(promises);
 
-      for (let i = 0; i < batcheduserAddresses.length; i++) {
+      for (let i = 0; i < batchedUserAddresses.length; i++) {
         let updateData = {
-          ethereum_address: batcheduserAddresses[i],
+          ethereum_address: batchedUserAddresses[i],
           settle_amount: promiseResponses[i].data.balance,
           un_settled_debit_amount: '0',
           pessimistic_settled_balance: promiseResponses[i].data.balance
@@ -132,7 +188,7 @@ SyncDdbBalancesWithChain.prototype = {
 
 // const jsonStr = '{"0x4F739d40dB7509Fac8789a9Ff7bd3f86132738E2":["0xdcb2ec51c8c4632ae0039befef861fd1de2cd616","0xb9f6d4c885e8653f66fbf4f1a87d2db0835c70e8","0x16b8e5c5cf4532969746d9c97c5c233b3647c105","0x06c18b69b13b38a2ed0d0c1146c2ed340ed4e7e5","0x8837741e2ede243927679e2b9c290243140dafc7","0x8187f120d3d2aa0fd32df04bc1cf8a148beadf1f","0x924efb75845b8a5b93e00f864391894360315bdb","0x79f720d773619edea6aa91ce9553983bf36ce87f","0xab3e3db1362b2b91663b22ff277f039eaf1b8cdb","0xfec3033dd1e9de348e13169ed7c53dd6e03b28a1","0xb5c0d3fc77ec5422c0a16cdf652c764d29df5961","0x53dd4f876e922877ba57921ee2f8200bc4ce9963","0x1cb5860593729a7ff4d0ca7d0bc101a4544b19a9"]}';
 const erc20AddressToAdressesMapStr =
-  '{"0x841f82a034d7a0453d915b11574b10afe0be35eb": ["0x069d4cd6bf876772ec474c0b2ef013bd93b0892f"]}';
+  '{"0x8a5a8cf3913d6886053d2e7217e13d26decd00d6": ["0x88fc4c810b9d83ffff4a791a484e8003905aae97","0x3fe697ff4655288086864492f1307eef32979b76"]}';
 const object = new SyncDdbBalancesWithChain(JSON.parse(erc20AddressToAdressesMapStr));
 object
   .perform()
