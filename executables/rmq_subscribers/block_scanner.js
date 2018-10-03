19793d12798d7efd9637c7bbf209fcf8b83b315a
Dhananjay8 <dhananjay@ost.com>
2018-10-03 16:54:06 +0530

    block scanner fixes for timestamp

--- executables/rmq_subscribers/block_scanner.js
+++ executables/rmq_subscribers/block_scanner.js
@@ -5,7 +5,8 @@ const rootPrefix = '../..';
 const args = process.argv,
   processLockId = args[2],
   group_id = args[3],
-  prefetchCountStr = args[4];
+  prefetchCountStr = args[4],
+  benchmarkFilePath = args[5];
 
 const logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
   StrategyByGroupHelper = require(rootPrefix + '/helpers/config_strategy/by_group_id'),
@@ -13,11 +14,48 @@ const logger = require(rootPrefix + '/lib/logger/custom_console_logger'),
   ProcessLockerKlass = require(rootPrefix + '/lib/process_locker'),
   ProcessLocker = new ProcessLockerKlass();
 
-require(rootPrefix + '/lib/block_scanner/block_scanner');
+require(rootPrefix + '/lib/block_scanner/for_tx_status_and_balance_sync');
 
 // Load external packages
 const openSTNotification = require('@openstfoundation/openst-notification'),
   OSTBase = require('@openstfoundation/openst-base');
+
+// Usage demo.
+const usageDemo = function() {
+  logger.log(
+    'usage:',
+    'node ./executables/rmq_subscribers/block_scanner.js processLockId group_id prefetchCountStr [benchmarkFilePath]'
+  );
+  logger.log(
+    '* processLockId is used for ensuring that no other process with the same processLockId can run on a given machine.'
+  );
+  logger.log('* group_id is needed to fetch config strategy.');
+  logger.log('* benchmarkFilePath is the path to the file which is storing the benchmarking info.');
+};
+
+// Validate and sanitize the command line arguments.
+const validateAndSanitize = function() {
+  if (!processLockId) {
+    logger.error('Process Lock id NOT passed in the arguments.');
+    usageDemo();
+    process.exit(1);
+  }
+
+  if (!group_id) {
+    logger.error('group_id is not passed');
+    usageDemo();
+    process.exit(1);
+  }
+
+  if (!prefetchCountStr) {
+    logger.error('prefetchCountStr is not passed');
+    usageDemo();
+    process.exit(1);
+  }
+};
+
+// Validate and sanitize the input params.
+validateAndSanitize();
 
 // Check if another process with the same title is running.
 ProcessLocker.canStartProcess({ process_title: 'executables_block_scanner_execute_transaction' + processLockId });
@@ -45,7 +83,8 @@ const promiseExecutor = async function(onResolve, onReject, params) {
       blockNumber: payload.blockNumber,
       provider: payload.provider,
       transactionHashes: payload.transactionHashes,
-      timeStamp: payload.timeStamp
+      timeStamp: payload.timestamp,
+      benchmarkFilePath: benchmarkFilePath
     });
 
   try {
@@ -91,7 +130,7 @@ openSTNotification.subscribeEvent.rabbit(
   {
     queue: 'block_scanner_execute_' + group_id,
     ackRequired: 1,
-    prefetch: prefetchCount
+    prefetch: 1
   },
   function(params) {
     // Promise is required to be returned to manually ack messages in RMQ
