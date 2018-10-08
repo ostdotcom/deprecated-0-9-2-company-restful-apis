b30421c828089071bce41e5999c0bd5316caf98d
alpeshvmodi <alpeshvmodi@gmail.com>
2018-10-08 17:53:17 +0530

    init execute tx fix & stale channel fix.
    (cherry picked from commit 2939bf8)

--- executables/rmq_subscribers/execute_transaction.js
+++ executables/rmq_subscribers/execute_transaction.js
@@ -33,7 +33,8 @@ let unAckCount = 0,
   processDetails = null,
   unAckCommandMessages = 0,
   txQueueSubscribed = false,
-  commandQueueSubscribed = false;
+  commandQueueSubscribed = false,
+  intentToConsumerTagMap = { cmdQueue: null, exTxQueue: null };
 
 // Start process locker.
 ProcessLocker.canStartProcess({
@@ -74,12 +75,8 @@ const commandResponseActions = async function(commandProcessorResponse) {
     commandProcessorResponse.data.shouldStopTxQueConsume &&
     commandProcessorResponse.data.shouldStopTxQueConsume === 1
   ) {
-    process.emit('CANCEL_CONSUME');
+    process.emit('CANCEL_CONSUME', intentToConsumerTagMap.exTxQueue);
     txQueueSubscribed = false;
-    commandQueueSubscribed = false;
-    setTimeout(async function() {
-      await subscribeCommandQueue(processDetails.queue_name_suffix, processDetails.chain_id);
-    }, 500);
   }
 };
 
@@ -191,18 +188,25 @@ const PromiseQueueManager = new OSTBase.OSTPromise.QueueManager(promiseTxExecuto
  */
 const subscribeTxQueue = async function(qNameSuffix, chainId) {
   if (!txQueueSubscribed) {
-    await openSTNotification.subscribeEvent.rabbit(
-      [rmqQueueConstants.executeTxTopicPrefix + chainId + '.' + qNameSuffix],
-      {
-        queue: rmqQueueConstants.executeTxQueuePrefix + '_' + chainId + '_' + qNameSuffix,
-        ackRequired: 1,
-        prefetch: txQueuePrefetchCount
-      },
-      function(params) {
-        // Promise is required to be returned to manually ack messages in RMQ
-        return PromiseQueueManager.createPromise(params);
-      }
-    );
+    if (intentToConsumerTagMap.exTxQueue) {
+      process.emit('RESUME_CONSUME', intentToConsumerTagMap.exTxQueue);
+    } else {
+      await openSTNotification.subscribeEvent.rabbit(
+        [rmqQueueConstants.executeTxTopicPrefix + chainId + '.' + qNameSuffix],
+        {
+          queue: rmqQueueConstants.executeTxQueuePrefix + '_' + chainId + '_' + qNameSuffix,
+          ackRequired: 1,
+          prefetch: txQueuePrefetchCount
+        },
+        function(params) {
+          // Promise is required to be returned to manually ack messages in RMQ
+          return PromiseQueueManager.createPromise(params);
+        },
+        function(consumerTag) {
+          intentToConsumerTagMap.exTxQueue = consumerTag;
+        }
+      );
+    }
     txQueueSubscribed = true;
   }
 };
@@ -245,6 +249,9 @@ const subscribeCommandQueue = async function(qNameSuffix, chainId) {
         unAckCommandMessages++;
         // Promise is required to be returned to manually ack messages in RMQ
         return commandQueueExecutor(params);
+      },
+      function(consumerTag) {
+        intentToConsumerTagMap.cmdQueue = consumerTag;
       }
     );
     commandQueueSubscribed = true;
