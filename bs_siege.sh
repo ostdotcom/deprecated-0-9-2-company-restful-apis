d3b79aa0a15458a216b67976a699ff6879ee7a37
Dhananjay8 <dhananjay@ost.com>
2018-10-10 14:35:22 +0530

    review changes

---
+++ bs_siege.sh
@@ -0,0 +1,3 @@
+node executables/rmq_subscribers/block_scanner.js 1 197 1 /mnt/st-company/apps/saasApi/current/log/worker_bs.csv &
+node executables/rmq_subscribers/block_scanner.js 2 197 1 /mnt/st-company/apps/saasApi/current/log/worker_bs.csv &
+node executables/rmq_subscribers/block_scanner.js 3 197 1 /mnt/st-company/apps/saasApi/current/log/worker_bs.csv &
