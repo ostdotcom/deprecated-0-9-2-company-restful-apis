eb37101e4ac2abf674b228e1b78160b33c1a1283
santhu210 <santhosh.nitdgp2015@gmail.com>
2018-10-24 15:41:36 +0530

    Added support for the caller to indicate when to exitw

--- executables/sigint_handler.js
+++ executables/sigint_handler.js
@@ -10,17 +10,31 @@ const SigIntHandler = function() {
 };
 
 SigIntHandler.prototype = {
+  /**
+   * attachHandlers - Attach SIGINT/SIGTERM handlers to the current process
+   *
+   */
   attachHandlers: function() {
     const oThis = this;
 
     let handle = function() {
-      const oThis = this; // Note, this is a process scope
-
-      process.exit(1);
+      if (oThis.pendingTasksDone()) {
+        process.exit(1);
+      }
+      logger.info(':: There are pending tasks. Waiting for completion.');
+      setTimeout(handle, 1000);
     };
 
     process.on('SIGINT', handle);
     process.on('SIGTERM', handle);
+  },
+
+  /**
+   * pendingTasksDone - Provides info whether the process has to exit
+   *
+   */
+  pendingTasksDone: function() {
+    throw 'pendingTasksDone method should be implemented by the caller for SIGINT handling';
   }
 };
 
