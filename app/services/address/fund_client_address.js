11b36b41ce3405b1ebe3b361ee1f27efc2cc486a
AnaghaMurtarkar <anagha@ost.com>
2018-11-21 17:11:35 +0530

    changes related to set initialGasTransferProperty

--- app/services/address/fund_client_address.js
+++ app/services/address/fund_client_address.js
@@ -176,7 +176,8 @@ FundClientAddressKlass.prototype = {
             .fire())[0];
 
           let newPropertiesValue = new ClientWorkerManagedAddressIdModel().setBit(
-            clientWorkerManagedAddressConst.hasStPrimeBalanceProperty,
+            clientWorkerManagedAddressConst.hasStPrimeBalanceProperty &&
+              clientWorkerManagedAddressConst.initialGasTransferredProperty,
             dbObject.properties
           );
 
